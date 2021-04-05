ebdae0a45c082fa2cbe128b346475304614780fa
andro <anonyos.mitra@gmail.com>
2021-04-05 15:42:42 +0530

    A

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import dbHandler as dbh
-print dbh.getTable("Mcity,Mstate,Mcountry,Mcontinent",["cityName","stateName","countryName","continentName"],"cityName=\"Bengaluru\"",{"stateId":"Mstate.id","Mstate.countryId":"Mcountry.id","continentId":"Mcontinent.id"})
+print (dbh.getTable("Mcity,Mstate,Mcountry,Mcontinent",["cityName","stateName","countryName","continentName"],"cityName=\"Bengaluru\"",{"stateId":"Mstate.id","Mstate.countryId":"Mcountry.id","continentId":"Mcontinent.id"}))
 con=dbh.Connect()
-print con.getTable("Mcity,Mstate,Mcountry,Mcontinent",["cityName","stateName","countryName","continentName"],"cityName=\"Bengaluru\"",{"stateId":"Mstate.id","Mstate.countryId":"Mcountry.id","continentId":"Mcontinent.id"})
+print(con.getTable("Mcity,Mstate,Mcountry,Mcontinent",["cityName","stateName","countryName","continentName"],"cityName=\"Bengaluru\"",{"stateId":"Mstate.id","Mstate.countryId":"Mcountry.id","continentId":"Mcontinent.id"}))
 con.close()
