d78e07023a013576531619b708d359afec574afd
andro <anonyos.mitra@gmail.com>
2021-04-05 18:14:20 +0530

    A

--- static/js/tabsPage.js
+++ static/js/tabsPage.js
@@ -5,10 +5,8 @@ function tabsPage_init(){
 }
 function tabsPage_openTab(tabName){
     tabs=["Country","State","City"];
-    if(tabName in tabs && tabName!=cache["tabName"])
+    if((tabName in tabs)&&(tabName!=cache["tabName"]))
         POST({"tab":tabName},"openTab")
-    else
-    console.log("error")
 }
 var getApp = new XMLHttpRequest();
     getApp.onreadystatechange = function () {
