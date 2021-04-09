d1ccd69e78e54079597fad13ebc0544ee55d3715
andro <anonyos.mitra@gmail.com>
2021-04-09 15:55:23 +0530

    A

--- static/js/tabsPage.js
+++ static/js/tabsPage.js
@@ -158,5 +158,5 @@ function submitForm(id)
     }
 function deleteEntry(id)
     {
-        POST({"id":id,"tab":cache["tabName"]},"deleteEntry");
+        POST({"id":id,"tab":cache["tabName"]},"deleteEntry","token":cache["token"]);
     }
