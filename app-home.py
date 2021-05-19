0b63d36fa7bd22eb4bf4e47bb6167e5e4908c95f
andro <anonyos.mitra@gmail.com>
2021-05-19 14:40:21 +0530

    A

--- app-home.py
+++ app-home.py
@@ -119,19 +119,14 @@ def internal_error(exception):
     etype, value, tb = sys.exc_info()
     print(traceback.print_exception(etype, value, tb))"""
 
-@app.errorhandler(HTTPException)
+@app.errorhandler(Exception)
 def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    # start with the correct headers and status code from the error
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
+    # pass through HTTP errors. You wouldn't want to handle these generically.
+    if isinstance(e, HTTPException):
+        return e
+
+    # now you're handling non-HTTP exceptions only
+    return "Error", 500
 
 @app.route('/test', methods=['GET'])
 def test():
