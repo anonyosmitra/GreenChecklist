3a813d1a010202cb7ed2fc57f7812bee069dcec0
andro <anonyos.mitra@gmail.com>
2021-05-19 14:42:46 +0530

    A

--- app-home.py
+++ app-home.py
@@ -121,7 +121,7 @@ def internal_error(exception):
 @app.errorhandler(Exception)
 def handle_exception(e):
     # pass through HTTP errors. You wouldn't want to handle these generically.
-    internal_error(e)
+    logError(e)
     if isinstance(e, HTTPException):
         return e
 
